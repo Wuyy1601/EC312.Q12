320c04c4cc63b5df715097a8c88ef9724e255c92
thhdang16 <thhdang16@gmail.com>
2025-11-11 08:41:46 +0700

    Get User by Id

--- BackEnd/src/routes/userRouters.js
+++ BackEnd/src/routes/userRouters.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getAllUsers } from "../controllers/userController.js";
+import { getAllUsers, getUserById } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // GET /api/users - Lấy tất cả users
 router.get("/", getAllUsers);
 
+// GET /api/users/:id - Lấy thông tin user theo ID
+router.get("/:id", getUserById);
+
 export default router;
+
+
+// PUT /api/users/:id - Cập nhật thông tin user theo ID
+// DELETE /api/users/:id - Xóa user theo ID
